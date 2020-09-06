75cd3f2923dc707279e844ea82af460e3bfc34b3
Benj Tupas <benj.tupas.products@gmail.com>
2020-09-06 18:10:14 +0800

    Campaign contract

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-Goals of the project:
+# Goals of the project:
+
 - Create your own token based of Ethereum
 - Launch your own Token Raise
+
+# Project Guide:
+
+1. app
+
+- The web app for our token raise built on React and Node JS.
+- This will communicate to our Ethereum Smart Contracts
+
+2. ethereum
+
+- This is where we code our Smart Contracts on top of Ethereum
+
+# Steps to create app:
+
+- Install NodeJS
+- cd YOUR_PROJECT_FOLDER
+- npm init
+- npm install --save ganache-cli mocha solc fs-extra web3@1.0.0-beta.35
+
+# Resources
+
+1. [Ethereum and Solidity: The Complete Developer's Guide](https://www.udemy.com/course/ethereum-and-solidity-the-complete-developers-guide) on Udemy
---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,93 @@
+pragma solidity ^0.4.17;
+
+contract CampaignContractGenerator {
+    address[] public campaigns;
+
+    function create() public {
+        address campaign = new Campaign(msg.sender);
+        campaigns.push(campaign);
+    }
+
+    function getCampaigns() public view returns (address[]) {
+        return campaigns;
+    }
+}
+
+contract Campaign {
+
+    struct Request {
+        address recipient;
+        uint value;
+        string description;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) investorsWhoApproved;
+    }
+
+    address public manager;
+    Request[] public requests;
+    mapping(address => bool) public investors;
+    uint numberOfInvestors;
+
+    function Campaign(address creator) public {
+        manager = creator;
+    }
+
+    function invest() public payable restrictManager {
+        require(msg.value >= 1 ether);
+
+        investors[msg.sender] = true;
+        manager.transfer(address(this).balance);
+        numberOfInvestors++;
+    }
+
+    function createRequest( address recipient,
+                            uint value,
+                            string description) public accessManager {
+
+        Request memory request = Request({
+            recipient: recipient,
+            value: value,
+            description: description,
+            complete: false,
+            approvalCount: 0
+        });
+
+        requests.push(request);
+    }
+
+    function approveRequest(uint index) public accessInvestors {
+        Request storage request = requests[index];
+
+        require(!request.investorsWhoApproved[msg.sender]);
+
+        request.investorsWhoApproved[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public accessManager {
+        Request storage request = requests[index];
+
+        require(request.approvalCount > (numberOfInvestors / 2));
+        require(!request.complete);
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+
+    // Access
+    modifier accessManager() {
+        require(manager == msg.sender);
+        _;
+    }
+
+    modifier restrictManager() {
+        require(manager != msg.sender);
+        _;
+    }
+
+    modifier accessInvestors() {
+        require(investors[msg.sender]);
+        _;
+    }
+}
